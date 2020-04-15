aa0a063d79f5a56251119cf0e03991c81d5d48c9
Fidel Thomet <me@fidelthomet.com>
2020-04-15 23:06:20 +0200

    [Update] removed scrollama

---
+++ src/components/IntersectionObserver.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="intersection-observer" ref="intersect" :class="{ default: !noStyling }">
+    <slot>
+      <h2>Step {{ step }} – {{ step + stepSize }}</h2>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intersection-observer',
+  props: {
+    step: {
+      type: Number,
+      default: 0
+    },
+    stepSize: {
+      type: Number,
+      default: 1
+    },
+    observerOptions: {
+      type: Object,
+      default () {
+        return {
+          rootMargin: `-${0.5 * 100}% 0% -${100 - 0.5 * 100}% 0%`
+        }
+      }
+    },
+    noStyling: {
+      type: Boolean,
+      default: false
+    },
+    align: {
+      type: String,
+      default: 'center'
+    }
+  },
+  data () {
+    return {
+      active: false
+    }
+  },
+  mounted () {
+    const { $refs, observerOptions } = this
+    const observer = new window.IntersectionObserver(entries => {
+      if (entries.filter(entry => entry.isIntersecting).length < 1) {
+        this.active = false
+        return
+      }
+      requestAnimationFrame(this.watch)
+      this.active = true
+    }, observerOptions)
+
+    observer.observe($refs.intersect)
+  },
+  methods: {
+    watch () {
+      const { active, $refs, step, stepSize } = this
+      const rect = $refs.intersect.getBoundingClientRect()
+      const offset = innerHeight * 0.5
+      const scroll = Math.max(Math.min((offset - rect.top) / rect.height, 1), 0)
+      this.$parent.$emit('step', { step, progress: scroll * stepSize })
+      if (active) requestAnimationFrame(this.watch)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/style/global";
+
+.intersection-observer {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/MdRenderer.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="el" class="md-renderer" v-html="html"/>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it'
+const md = new MarkdownIt()
+export default {
+  name: 'md-renderer',
+  props: {
+    text: {
+      type: [String, Array],
+      default: null
+    },
+    el: {
+      type: String,
+      default: 'div'
+    }
+  },
+  computed: {
+    html () {
+      const { text } = this
+      return md.render(typeof text === 'string' ? text : text.join('\n\n'))
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/style/global";
+.md-renderer {
+  width: 100%;
+  max-width: $max-width;
+}
+</style>
